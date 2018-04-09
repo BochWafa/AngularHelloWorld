9ce03d6dc7d52f249ffd22377ccf65128b4c90ba
BochWafa <wafa.bouchami@yahoo.fr>
2018-04-09 22:04:32 +0100

    6th updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Install express server
 const express = require('express');
 const app = express();
+app.set('port', (process.env.PORT || 5000));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
